0a92a2a3f908d27f88d7e9800dc75ffe9a81ca72
Ron MacNeil <macro@hotmail.com>
2007-11-07 08:23:17 +0000

    Context  - Add rough cut of Com.Halfdecent.Context, with initial support for tracking:      - Calls      - Parameters      - Property

---
+++ Com.Halfdecent.System/src/Context/IParameterContext.cs
@@ -0,0 +1,54 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2007 Ron MacNeil <macro187 AT users DOT sourceforge DOT net>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// -----------------------------------------------------------------------------
+
+
+using System;
+using Com.Halfdecent.System;
+
+
+namespace
+Com.Halfdecent.Context
+{
+
+
+
+
+/// Indicates that an item was passed as a particular parameter
+public interface
+IParameterContext
+    : INameContext
+{
+
+
+
+
+// -----------------------------------------------------------------------------
+// Properties
+// -----------------------------------------------------------------------------
+
+/// The position of the parameter the item was passed as
+int
+Position
+{
+    get;
+}
+
+
+
+
+} // type
+} // namespace
+
