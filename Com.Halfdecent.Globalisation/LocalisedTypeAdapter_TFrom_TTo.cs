212a699bec907e4e8bfb72f03fd650d71438a33d
Ron MacNeil <macro@hotmail.com>
2008-11-25 03:21:58 +0000

    ACTUALLY add LocalisedTypeAdapter

---
+++ Com.Halfdecent.Globalisation/LocalisedTypeAdapter_TFrom_TTo.cs
@@ -0,0 +1,89 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2008 Ron MacNeil <macro187 AT users DOT sourceforge DOT net>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// -----------------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+
+namespace
+Com.Halfdecent.Globalisation
+{
+
+// =============================================================================
+/// Localised< T > widening type adapter
+// =============================================================================
+
+public class
+LocalisedTypeAdapter<
+    TFrom,
+    TTo
+>
+    :Localised< TTo >
+    where TFrom : TTo
+{
+
+
+
+
+// -----------------------------------------------------------------------------
+// Constructors
+// -----------------------------------------------------------------------------
+
+public
+LocalisedTypeAdapter(
+    Localised< TFrom > from
+)
+{
+    if( from == null ) throw new ArgumentNullException( "from" );
+    this.from = from;
+}
+
+
+
+
+// -----------------------------------------------------------------------------
+// Properties
+// -----------------------------------------------------------------------------
+
+public
+Localised< TFrom >
+From { get { return this.from; } }
+
+private
+Localised< TFrom >
+from;
+
+
+
+
+// -----------------------------------------------------------------------------
+// Localised< TTo >
+// -----------------------------------------------------------------------------
+
+protected override
+TTo
+ForCulture(
+    CultureInfo culture
+)
+{
+    return this.From[ culture ];
+}
+
+
+
+
+} // type
+} // namespace
+
