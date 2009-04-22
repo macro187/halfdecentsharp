1fb559bfcca69f4ea248de4025dd7cb90a7e4c08
Ron MacNeil <macro@hotmail.com>
2009-04-22 03:55:57 +0000

    Meta: ArgumentOutOfRangeValueException

--- Com.Halfdecent.Meta.Test/Tests.cs
+++ Com.Halfdecent.Meta.Test/Tests.cs
@@ -242,6 +242,76 @@ Test_ArgumentNullValueException()
 }
 
 
+[Test( "ArgumentOutOfRangeValueException" )]
+public static
+void
+Test_ArgumentOutOfRangeValueException()
+{
+    Parameter           parameterReference = new Parameter( "fakeparam" );
+    Localised< string > messageFormat = "Fake problem with {0}";
+    Exception           innerException = new Exception();
+    Localised< string > reference = "some fake parameter";
+
+    ArgumentOutOfRangeValueException e;
+    Print( "Using ArgumentOutOfRangeValueException( parameterReference )..." );
+    e = new ArgumentOutOfRangeValueException( parameterReference );
+    Print( ".ParameterReference" );
+    AssertEqual( e.ParameterReference, parameterReference );
+    Print( ".MessageFormat" );
+    Print( "\"{0}\"", e.MessageFormat );
+    Print( ".ValueReference" );
+    AssertEqual( e.ValueReference, parameterReference );
+    Print( ".SayMessage()" );
+    Print( "\"{0}\"", e.SayMessage( reference ) );
+    Print( ".Message" );
+    Print( "\"{0}\"", e.Message );
+    Print( ".InnerException" );
+    AssertEqual( e.InnerException, null );
+
+    Print( "Using ArgumentOutOfRangeValueException( parameterReference, messageFormat )..." );
+    e = new ArgumentOutOfRangeValueException( parameterReference, messageFormat );
+    Print( ".ParameterReference" );
+    AssertEqual( e.ParameterReference, parameterReference );
+    Print( ".MessageFormat" );
+    AssertEqual< string >( e.MessageFormat, messageFormat );
+    Print( ".ValueReference" );
+    AssertEqual( e.ValueReference, parameterReference );
+    Print( ".SayMessage()" );
+    AssertEqual< string >(
+        e.SayMessage( reference ),
+        LocalisedString.Format( messageFormat, reference) );
+    Print( ".Message" );
+    AssertEqual< string >(
+        e.Message,
+        LocalisedString.Format(
+            messageFormat,
+            parameterReference.ToString() ) );
+    Print( ".InnerException" );
+    AssertEqual( e.InnerException, null );
+
+    Print( "Using ArgumentOutOfRangeValueException( parameterReference, messageFormat, innerException )..." );
+    e = new ArgumentOutOfRangeValueException( parameterReference, messageFormat, innerException );
+    Print( ".ParameterReference" );
+    AssertEqual( e.ParameterReference, parameterReference );
+    Print( ".MessageFormat" );
+    AssertEqual< string >( e.MessageFormat, messageFormat );
+    Print( ".ValueReference" );
+    AssertEqual( e.ValueReference, parameterReference );
+    Print( ".SayMessage()" );
+    AssertEqual< string >(
+        e.SayMessage( reference ),
+        LocalisedString.Format( messageFormat, reference) );
+    Print( ".Message" );
+    AssertEqual< string >(
+        e.Message,
+        LocalisedString.Format(
+            messageFormat,
+            parameterReference.ToString() ) );
+    Print( ".InnerException" );
+    AssertEqual( e.InnerException, innerException );
+}
+
+
 
 
 } // type
