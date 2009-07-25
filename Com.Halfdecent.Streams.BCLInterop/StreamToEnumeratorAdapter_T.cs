51ded3d945cb4c000c9ce6a7f41764bf0043e49c
Ron MacNeil <macro@hotmail.com>
2009-07-25 18:57:08 +0000

    Streams.BCLInterop: _S() isn't being used right now

--- Com.Halfdecent.Streams.BCLInterop/StreamToEnumeratorAdapter_T.cs
+++ Com.Halfdecent.Streams.BCLInterop/StreamToEnumeratorAdapter_T.cs
@@ -88,7 +88,7 @@ MoveNext(
 
 
 
-private static Com.Halfdecent.Globalisation.Localised< string > _S( string s, params object[] args ) { return Com.Halfdecent.Resources.Resource._S( global::System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType, s, args ); }
+//private static Com.Halfdecent.Globalisation.Localised< string > _S( string s, params object[] args ) { return Com.Halfdecent.Resources.Resource._S( global::System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType, s, args ); }
 
 } // type
 } // namespace
