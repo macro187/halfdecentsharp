8d648e9d0f6497d5ac96b2e3153e548a1314094d
Ron MacNeil <macro@hotmail.com>
2010-03-22 01:46:32 +0000

    Cursors: Cursor*Proxy

---
+++ Com.Halfdecent.Cursors/CursorRCGProxy_T.cs
@@ -0,0 +1,95 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2010
+// Ron MacNeil <macro187 AT users DOT sourceforge DOT net>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// -----------------------------------------------------------------------------
+
+
+using Com.Halfdecent.Meta;
+using Com.Halfdecent.RTypes;
+using Com.Halfdecent.Numerics;
+
+
+namespace
+Com.Halfdecent.Cursors
+{
+
+
+public class
+CursorRCGProxy<
+    T
+>
+    : ICursorRCG< T >
+{
+
+
+
+public
+CursorRCGProxy(
+    ICursorRCG< T > from
+)
+{
+    new NonNull().Require( from, new Parameter( "from" ) );
+    this.From = from;
+}
+
+
+protected
+    ICursorRCG< T >
+From
+{
+    get;
+    private set;
+}
+
+
+
+// -----------------------------------------------------------------------------
+// ICursor
+// -----------------------------------------------------------------------------
+
+public bool AtBeginning { get { return this.From.AtBeginning; } }
+public bool AtEnd { get { return this.From.AtEnd; } }
+public IInteger TryMove( IInteger count) { return this.From.TryMove( count ); }
+
+
+
+// -----------------------------------------------------------------------------
+// ICursorR
+// -----------------------------------------------------------------------------
+
+public T Get() { return this.From.Get(); }
+
+
+
+// -----------------------------------------------------------------------------
+// ICursorC
+// -----------------------------------------------------------------------------
+
+public void Set( T replacement ) { this.From.Set( replacement ); }
+
+
+
+// -----------------------------------------------------------------------------
+// ICursorG
+// -----------------------------------------------------------------------------
+
+public void Insert( T replacement ) { this.From.Insert( replacement ); }
+
+
+
+
+} // type
+} // namespace
+
