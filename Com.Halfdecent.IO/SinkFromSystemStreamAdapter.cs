dbeb0be6bf7551d33397026e15523125d1ad0d18
Ron MacNeil <macro@hotmail.com>
2010-07-06 05:18:09 +0000

    Introducing Com.Halfdecent.IO - System.IO.Stream::AsHalfdecentStream() - System.IO.Stream::AsHalfdecentSink()

---
+++ Com.Halfdecent.IO/SinkFromSystemStreamAdapter.cs
@@ -0,0 +1,90 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2010
+// Ron MacNeil <macro187 AT users DOT sourceforge DOT net>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// -----------------------------------------------------------------------------
+
+
+using Com.Halfdecent;
+using Com.Halfdecent.Meta;
+using Com.Halfdecent.RTypes;
+using Com.Halfdecent.Streams;
+
+
+namespace
+Com.Halfdecent.IO
+{
+
+
+public class
+SinkFromSystemStreamAdapter
+    : ISink< byte >
+{
+
+
+
+// -----------------------------------------------------------------------------
+// Constructors
+// -----------------------------------------------------------------------------
+
+public
+SinkFromSystemStreamAdapter(
+    System.IO.Stream from
+)
+{
+    new NonNull().Check( from, new Parameter( "from" ) );
+    if( !from.CanWrite ) throw new ValueArgumentException(
+        new Parameter( "from" ),
+        _S("{0} is not a writeable stream") );
+    this.From = from;
+}
+
+
+
+// -----------------------------------------------------------------------------
+// Properties
+// -----------------------------------------------------------------------------
+
+protected
+System.IO.Stream
+From
+{
+    get;
+    private set;
+}
+
+
+
+// -----------------------------------------------------------------------------
+// ISink< byte >
+// -----------------------------------------------------------------------------
+
+public
+    bool
+TryPush(
+    byte item
+)
+{
+    this.From.WriteByte( item );
+    return true;
+}
+
+
+
+
+private static global::Com.Halfdecent.Globalisation.Localised< string > _S( string s, params object[] args ) { return global::Com.Halfdecent.Resources.Resource._S( global::System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType, s, args ); }
+
+} // type
+} // namespace
+
