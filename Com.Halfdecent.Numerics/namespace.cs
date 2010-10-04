f72ae8261ab49c5d14c723ce7ccb8da97e6721d2
Ron MacNeil <macro@hotmail.com>
2010-10-04 03:40:28 +0000

    Numerics: Namespace-wide documentation

--- Com.Halfdecent.Numerics/namespace.cs
+++ Com.Halfdecent.Numerics/namespace.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------------
-// Copyright (c) 2008, 2009
+// Copyright (c) 2008, 2009, 2010
 // Ron MacNeil <macro187 AT users DOT sourceforge DOT net>
 //
 // Permission to use, copy, modify, and distribute this software for any
@@ -19,30 +19,48 @@
 // =============================================================================
 /// Numbers and arithmetic
 ///
-/// Abstract numeric types
-/// (see <tt>http://en.wikipedia.org/wiki/Numerical_tower</tt>)
-/// - <tt>IReal</tt>
-/// - <tt>IInteger</tt>
 ///
-/// Implementations using <tt>System.Decimal</tt>
-/// - <tt>DecimalReal</tt>
-/// - <tt>DecimalInteger</tt>
+/// @section problem Problem
 ///
-/// RTypes
-/// - <tt>NonZero</tt>
-/// - <tt>NonNegative</tt>
-/// - <tt>NonFractional</tt>
+///     -   There are no abstract numeric types in the Base Class Library.
+///         When modelling entities that have nothing to do with computers,
+///         programmers are still forced to choose from a bunch of
+///         machine-specific numeric types.
 ///
-/// Base class library numeric type range RTypes
-/// - <tt>InDecimalRange</tt>
-/// - <tt>InUInt64Range</tt>
-/// - <tt>InInt64Range</tt>
-/// - <tt>InUInt32Range</tt>
-/// - <tt>InInt32Range</tt>
-/// - <tt>InUInt16Range</tt>
-/// - <tt>InInt16Range</tt>
-/// - <tt>InByteRange</tt>
-/// - <tt>InSByteRange</tt>
+///     -   The Base Class Library numeric types have nothing in common from a
+///         type perspective, even though they're clearly related.  Among other
+///         things, this makes it impossible to create generic numeric
+///         algorithms that work with more than one numeric type.
+///
+///
+/// @section solution Solution
+///
+///     -   Abstract numeric types
+///         (see <tt>http://en.wikipedia.org/wiki/Numerical_tower</tt>)
+///         - <tt>IReal</tt>
+///         - <tt>IInteger</tt>
+///
+///     -   Implementations using <tt>System.Decimal</tt>
+///         - <tt>DecimalReal</tt>
+///         - <tt>DecimalInteger</tt>
+///
+///     -   RTypes
+///         - <tt>NonZero</tt>
+///         - <tt>NonNegative</tt>
+///         - <tt>NonFractional</tt>
+///
+///     -   Base class library numeric type range RTypes
+///         - <tt>InDecimalRange</tt>
+///         - <tt>InUInt64Range</tt>
+///         - <tt>InInt64Range</tt>
+///         - <tt>InUInt32Range</tt>
+///         - <tt>InInt32Range</tt>
+///         - <tt>InUInt16Range</tt>
+///         - <tt>InInt16Range</tt>
+///         - <tt>InByteRange</tt>
+///         - <tt>InSByteRange</tt>
+///
+///
 // =============================================================================
 
 namespace
