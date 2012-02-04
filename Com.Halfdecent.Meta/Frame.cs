529c7afa207c44f35f646a471d588a43d43af880
Ron MacNeil <macro@hotmail.com>
2012-02-04 07:32:57 +0000

    Meta: Adapt to recent Resources and Globalisation changes

--- Com.Halfdecent.Meta/Frame.cs
+++ Com.Halfdecent.Meta/Frame.cs
@@ -225,7 +225,7 @@ Equals(
 
 
 
-private static Com.Halfdecent.Globalisation.Localised< string > _S( string s, params object[] args ) { return Com.Halfdecent.Resources.Resource._S( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType, s, args ); }
+private static Com.Halfdecent.Globalisation.Localised< string > _S( string s, params object[] args ) { return Com.Halfdecent.Globalisation.LocalisedResource._S( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType, s, args ); }
 
 } // type
 } // namespace
--- Com.Halfdecent.Meta/ValueReferenceException.cs
+++ Com.Halfdecent.Meta/ValueReferenceException.cs
@@ -323,7 +323,7 @@ Message
 
 
 
-private static Com.Halfdecent.Globalisation.Localised< string > _S( string s, params object[] args ) { return Com.Halfdecent.Resources.Resource._S( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType, s, args ); }
+private static Com.Halfdecent.Globalisation.Localised< string > _S( string s, params object[] args ) { return Com.Halfdecent.Globalisation.LocalisedResource._S( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType, s, args ); }
 
 } // type
 } // namespace
