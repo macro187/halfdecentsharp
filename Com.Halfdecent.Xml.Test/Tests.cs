74247ab33b47cd877b6c72097c494711c29acd54
Ron MacNeil <macro@hotmail.com>
2012-08-18 13:48:35 +1200

    Com.Halfdecent.Xml

---
+++ Com.Halfdecent.Xml.Test/Tests.cs
@@ -0,0 +1,155 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2012
+// Ron MacNeil <macro187 AT users DOT sourceforge DOT net>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// -----------------------------------------------------------------------------
+
+
+using Com.Halfdecent.Testing;
+using Com.Halfdecent.Xml;
+using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Com.Halfdecent;
+
+
+namespace
+Com.Halfdecent.Xml.Test
+{
+
+
+// =============================================================================
+/// Test program for <tt>Com.Halfdecent.Xml</tt>
+// =============================================================================
+//
+public class
+Tests
+    : TestBase
+{
+
+
+
+public static
+    int
+Main()
+{
+    return TestProgram.RunTests();
+}
+
+
+[Test( "Apply(XElement,XElement)" )]
+public static
+    void
+Test_Apply()
+{
+    var xml =
+        new XElement( "root",
+            new XAttribute( "a", "a" ),
+            new XElement( "a",
+                new XAttribute( "a", "a" ),
+                "atext" ));
+    SystemXDocument.Apply(
+        new XElement( "root",
+            new XAttribute( "b", "b" ),
+            new XElement( "b",
+                new XAttribute( "b", "b" ),
+                "btext" )),
+        xml );
+    Assert(
+        XNode.DeepEquals(
+            xml,
+            new XElement( "root",
+                new XAttribute( "a", "a" ),
+                new XElement( "a",
+                    new XAttribute( "a", "a" ),
+                    "atext" ),
+                new XAttribute( "b", "b" ),
+                new XElement( "b",
+                    new XAttribute( "b", "b" ),
+                    "btext" ))));
+}
+
+
+[Test( "Apply(XElement,XElement,ElementMatchRule.ByName())" )]
+public static
+    void
+Test_ApplyByName()
+{
+    var xml =
+        new XElement( "root",
+            new XElement( "a",
+                new XAttribute( "a", "a" )));
+    SystemXDocument.Apply(
+        new XElement( "root",
+            new XElement( "a",
+                new XAttribute( "b", "b" ),
+                "btext" )),
+        xml,
+        ElementMatchRule.ByName(
+            e => e.Elements( "a" ) ) );
+    Assert(
+        XNode.DeepEquals(
+            xml,
+            new XElement( "root",
+                new XElement( "a",
+                    new XAttribute( "a", "a" ),
+                    new XAttribute( "b", "b" ),
+                    "btext" ))));
+}
+
+
+[Test( "Apply(XElement,XElement,ElementMatchRule.ByNameAndAttributes())" )]
+public static
+    void
+Test_ApplyByNameAndAttributes()
+{
+    var xml =
+        new XElement( "root",
+            new XElement( "a",
+                new XAttribute( "id", "1" )),
+            new XElement( "a",
+                new XAttribute( "id", "2" )),
+            new XElement( "a",
+                new XAttribute( "id", "3" )));
+    SystemXDocument.Apply(
+        new XElement( "root",
+            new XElement( "a",
+                new XAttribute( "id", "2" ),
+                new XAttribute( "new", "new" ),
+                "newtext" )),
+        xml,
+        ElementMatchRule.ByNameAndAttributes(
+            e => e.Elements( "a" ),
+            "id" ) );
+    Assert(
+        XNode.DeepEquals(
+            xml,
+            new XElement( "root",
+                new XElement( "a",
+                    new XAttribute( "id", "1" )),
+                new XElement( "a",
+                    new XAttribute( "id", "2" ),
+                    new XAttribute( "new", "new" ),
+                    "newtext" ),
+                new XElement( "a",
+                    new XAttribute( "id", "3" )))));
+}
+
+
+
+
+} // type
+} // namespace
+
---
+++ Com.Halfdecent.Xml/ElementMatchRule.cs
@@ -0,0 +1,169 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2012
+// Ron MacNeil <macro187 AT users DOT sourceforge DOT net>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// -----------------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace
+Com.Halfdecent.Xml
+{
+
+
+// =============================================================================
+/// A rule explaining, for certain elements in an XML tree, how to match the
+/// "same" element in another
+// =============================================================================
+
+public class
+ElementMatchRule
+{
+
+
+
+// -----------------------------------------------------------------------------
+// Factory Methods
+// -----------------------------------------------------------------------------
+
+/// Create a new element match rule that matches by element name
+///
+public static
+    ElementMatchRule
+ByName(
+    Func< XElement, IEnumerable< XElement > > appliesToFunc
+)
+{
+    return ByFunction(
+        appliesToFunc,
+        (e1,e2) => e2.Name == e1.Name );
+}
+
+
+/// Create a new element match rule that matches by element name plus the
+/// values of one or more attributes
+///
+public static
+    ElementMatchRule
+ByNameAndAttributes(
+    Func< XElement, IEnumerable< XElement > >   appliesToFunc,
+    params string[]                             attributeNames
+)
+{
+    attributeNames = attributeNames ?? new string[]{};
+    return ByFunction(
+        appliesToFunc,
+        (e1,e2) =>
+            e2.Name == e1.Name &&
+            attributeNames.All( n =>
+                (string)( e2.Attribute( n ) ) ==
+                (string)( e1.Attribute( n ) ) ) );
+}
+
+
+/// Create a new rule that matches using a match function
+///
+public static
+    ElementMatchRule
+ByFunction(
+    Func< XElement, IEnumerable< XElement > >   appliesToFunc,
+    Func< XElement, XElement, bool >            matchFunc
+)
+{
+    return new ElementMatchRule( appliesToFunc, matchFunc );
+}
+
+
+
+// -----------------------------------------------------------------------------
+// Constructors
+// -----------------------------------------------------------------------------
+
+private
+ElementMatchRule(
+    Func< XElement, IEnumerable< XElement > >   appliesToFunc,
+    Func< XElement, XElement, bool >            matchFunc
+)
+{
+    if( appliesToFunc == null )
+        throw new ArgumentNullException( "appliesToFunc" );
+    if( matchFunc == null )
+        throw new ArgumentNullException( "matchFunc" );
+    this.AppliesToFunc = appliesToFunc;
+    this.MatchFunc = matchFunc;
+}
+
+
+
+// -----------------------------------------------------------------------------
+// Private
+// -----------------------------------------------------------------------------
+
+private
+Func< XElement, IEnumerable< XElement > >
+AppliesToFunc;
+
+
+private
+Func< XElement, XElement, bool >
+MatchFunc;
+
+
+
+// -----------------------------------------------------------------------------
+// Methods
+// -----------------------------------------------------------------------------
+
+/// Given the root element of an XML tree, lists the elements this rule applies
+/// to
+///
+public
+    IEnumerable< XElement >
+AppliesTo(
+    XElement root
+)
+{
+    if( root == null )
+        throw new ArgumentNullException( "root" );
+    return this.AppliesToFunc( root );
+}
+
+
+/// Indicate whether two elements are the "same" according to the rule
+///
+public
+    bool
+Match(
+    XElement e1,
+    XElement e2
+)
+{
+    if( e1 == null )
+        throw new ArgumentNullException( "e1" );
+    if( e2 == null )
+        throw new ArgumentNullException( "e2" );
+    return MatchFunc( e1, e2 );
+}
+
+
+
+
+} // type
+} // namespace
+
